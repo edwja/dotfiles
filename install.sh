a034dcdaadf25df4eacac3d368bb30817fc97772
Andy Edwards <aedwards@merchantsbonding.com>
2022-02-19 10:20:41 -0600

    make install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This is idempotent, meaning you can run it over and over again without fear of
+# breaking anything. Use it as an installer or to upgrade after merging from an
+# upstream fork.
+
+for f in *; do
+  [[ "$f" == "" || "$f" =~ ^install.* || "$f" == "README.md" ]] && continue
+
+  echo "$PWD/$f -> ~/.$f"
+  ln -ns "$PWD/$f" "$HOME/.$f"
+done
+
+exit 0
